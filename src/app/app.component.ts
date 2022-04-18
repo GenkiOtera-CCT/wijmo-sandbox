74ef2dec32e89b1c5731818279f05eb820b34ec4
GenkiOtera-CCT <genki.otera@cct-inc.co.jp>
2022-04-18 15:20:56 +0900

    app.componentにURLを配置

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    <div class='route-list'>
+      <nav class='item'>
+        <a routerLink="/">Home</a>
+      </nav>
+      <nav class='item'>
+        <a routerLink="/lim-column">lim-column</a>
+      </nav>
+      <!-- <nav class='item'>
+        <a routerLink="/hero">hero(ngrx/data)</a>
+      </nav> -->
     </div>
     <wj-flex-grid [itemsSource]="gridData">
       <wj-flex-grid-column header="ID" binding="id" [width]="60"></wj-flex-grid-column>
@@ -19,7 +22,15 @@ import { Component } from '@angular/core';
     </wj-flex-grid>
     <router-outlet></router-outlet>
   `,
-  styles: []
+  styles: [`
+    .route-list{
+      display:flex;
+      padding:30px;
+    }
+    .item{
+      padding-right:15px;
+    }
+  `]
 })
 export class AppComponent {
   title = 'wijmo-sandbox';
