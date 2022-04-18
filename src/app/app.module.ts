6e5edd9dfd29b06f06788eb4f0c3344a6cb917b4
GenkiOtera-CCT <genki.otera@cct-inc.co.jp>
2022-04-18 16:12:48 +0900

    グリッド選択に関するコンポネントを実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { WjGridModule } from '@grapecity/wijmo.angular2.grid'
+import { WjGridModule } from '@grapecity/wijmo.angular2.grid';
+import { WjInputModule } from '@grapecity/wijmo.angular2.input';
 import '@grapecity/wijmo.cultures/wijmo.culture.ja';
 
 import { LimColumnComponent } from './components/lim-column/lim-column.component';
 import { DragRowComponent } from './components/drag-row/drag-row.component';
+import { SelectGridComponent } from './components/select-grid/select-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LimColumnComponent,
-    DragRowComponent
+    DragRowComponent,
+    SelectGridComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    WjGridModule
+    FormsModule,
+    WjGridModule,
+    WjInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
