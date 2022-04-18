592c3e484b305f157de451dc6daeb17f705c66fa
GenkiOtera-CCT <genki.otera@cct-inc.co.jp>
2022-04-18 14:12:05 +0900

    クイックスタートを実装
    https://devlog.grapecity.co.jp/wijmo-angular-quickstart/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WjGridModule } from '@grapecity/wijmo.angular2.grid'
+import '@grapecity/wijmo.cultures/wijmo.culture.ja';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WjGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
