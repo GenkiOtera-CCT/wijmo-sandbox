f0cdba7bbee848f8358f5312b8e98b14786b3ff5
GenkiOtera-CCT <genki.otera@cct-inc.co.jp>
2022-04-18 15:18:30 +0900

    列制御コンポネントをルーティング

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LimColumnComponent } from './components/lim-column/lim-column.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'lim-column', component: LimColumnComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
